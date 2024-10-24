7d78f2a687243ca68962804ccb693024f56d79d2
vferrand-smid <vferrand@smartphone-id.com>
2024-10-24 10:56:23 +0200

    lojikljh

--- next.config.mjs
+++ next.config.mjs
@@ -23,12 +23,6 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'dev.smartphone-id.com',
-                port: '',
-                pathname: '/wp-content/uploads/**',
-            },
-            {
-                protocol: 'https',
                 hostname: 'storage.googleapis.com',
                 pathname: '**',
             },
