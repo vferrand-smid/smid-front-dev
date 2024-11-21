f0de8a7ac40f653c0b7880ad601f89baeaad65e9
vferrand-smid <vferrand@smartphone-id.com>
2024-11-21 09:16:29 +0100

    modif script GTM

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -62,19 +62,6 @@ export default function RootLayout({children, params}) {
         <head>
             <title>{title}</title>
             <meta name="description" content={description}/>
-            {/* Both scripts with afterInteractive strategy */}
-            <Script id="gtm-init" strategy="afterInteractive">
-                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], 
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTM_ID}');`}
-            </Script>
-            <Script id="dataLayer-init" strategy="afterInteractive">
-                {`window.dataLayer = window.dataLayer || []; 
-                window.dataLayer.push({event: 'page_view', origin_country: '${originCountry}', ga_property_id: '${GTM_ID}'});`}
-            </Script>
-
             {/* Google Tag Manager */}
             <Script id="custom-script">
                 {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
