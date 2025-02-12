7384420836a847588004b82d10ab6386207f9058
vferrand-smid <vferrand@smartphone-id.com>
2025-02-12 17:12:49 +0100

    12.02 17:06 changement URL pour webApp

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -268,7 +268,7 @@ export default function RootLayout({children, params}) {
             n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
             if(!f._fbq) f._fbq=n; 
-            n.push=n;n.loaded=!0;n.version='2.0'; 
+            n.push=n;n.loaded=!0;n.version='2.0';
             n.queue=[]; 
             t=b.createElement(e); 
             t.async=!0;
